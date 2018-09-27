aa4cd16f5837c30dbea08c162dcae62b1a882b72
Nitvex <vozisovnikita@gmail.com>
2018-09-27 21:32:29 +0300

    [Add] lecture8 homework

---
+++ lecture8-homework/task.js
@@ -0,0 +1,15 @@
+Array.prototype.myMap = function (callback) {
+    let newArr = []
+    for (let i = 0; i < this.length; i++) {
+        newArr.push(callback(this[i], i, this));
+    }
+    return newArr;
+};
+
+console.log([1, 2, 3, 4].myMap((v, iter, arr) => {
+    return v * v;
+}));
+
+console.log([1, 2, 3, 4, 5, 6, 7].myMap((v, iter, arr) => {
+    return v + 1;
+}));
